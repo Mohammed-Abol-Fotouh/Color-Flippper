3a0dd1bdf0504506d05efe8cacdf27ac1426cdad
Mohammed-Abol-Fotouh <mohammedabolfotouh012@gmail.com>
2020-08-21 06:45:28 +0200

    color-flipper

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    // get random number from 0 - 3 from colors array
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    // change body background color
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
